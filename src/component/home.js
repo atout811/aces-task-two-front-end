ad182ab5214715aa297a8c496f4408eec00c2497
Mahmoud Youssef <mahmoud.yusof27@gmail.com>
2019-12-22 23:55:20 +0200

    decoding jsonwebtoken

--- src/component/home.js
+++ src/component/home.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import jwt from 'jsonwebtoken';
 
 export default class Home extends Component {
+  state = {
+    name: "",
+    email: "",
+    role: ""
+  };
+
+  componentDidMount(){
+    if(localStorage.getItem('x-auth-token')){
+      const decoded = jwt.decode(localStorage.getItem("x-auth-token"));
+      console.log(decoded.name);
+      this.setState({name: decoded.name, email: decoded.email, role: decoded.role});
+    }else{
+      this.setState({name: "", email: "", role: ""});
+    }
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Welcome To Our Task ^_^</h1>
+    // return (
+      let out = <div>
+  <h1>Welcome To Our Task ,{ this.state.name } ^_^</h1>
         <h3>
           if you're new here please <Link to="/signup">Sign up</Link>
         </h3>
@@ -13,6 +30,14 @@ export default class Home extends Component {
           if you have an account please <Link to="/login">Log in</Link>
         </h3>
       </div>
-    );
+
+      if(this.state.name){
+        out = <div>
+          <h1>Welcome To Our Task ^_^</h1>
+          <h3>Welcome, { this.state.name }</h3>
+        </div>
+      }
+      return (out);
+    // );
   }
 }
--- src/component/user.js
+++ src/component/user.js
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
+import jwt from "jsonwebtoken";
+import { Redirect } from "react-router-dom";
 
 export default class User extends Component {
+  state = {
+    name: "",
+    email: "",
+    role: ""
+  }
+  componentWillMount(){
+    if(localStorage.getItem('x-auth-token')){
+      let decoded = jwt.decode(localStorage.getItem("x-auth-token"));
+      this.setState({name: decoded.name, email: decoded.email, role: decoded.role});
+    }else{
+      this.setState({name: "", email: "", role: ""});
+    }
+  }
   render() {
+    if (!localStorage.getItem("x-auth-token")) {
+      return <Redirect to={{ pathname: "/" }} />;
+    }
     return (
       <div>
         <div className="ui two column grid">
           <div className="column">
-            <h2>Hesham</h2>
+            <h2>{ this.state.name }</h2>
           </div>
           <div className="column">
             <div className="ui raised segment">
-              <a className="ui red ribbon label">Overview</a>
+              <a className="ui red ribbon label" href="/">Overview</a>
               <span>Account Details</span>
-              <p></p>
-              <a className="ui blue ribbon label">Community</a> User Reviews
-              <p></p>
+              <p>{ this.state.email }</p>
+              <a className="ui blue ribbon label" href="/">Community</a> User Reviews
+              <p>{ this.state.role }</p>
             </div>
           </div>
         </div>
